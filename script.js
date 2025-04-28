0041d1712256afe10b515d074e2fdc5736d1d37d
Amina Mehwish <157866327+AminaMehwishkhan@users.noreply.github.com>
2025-04-28 06:35:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let numA = parseFloat(prompt("Enter first number:"));
+let numB = parseFloat(prompt("Enter second number:"));
+
+alert("Sum: " + (numA + numB) +
+      "\nDifference: " + (numA - numB) +
+      "\nProduct: " + (numA * numB) +
+      "\nQuotient: " + (numA / numB));
